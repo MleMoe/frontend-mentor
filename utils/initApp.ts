8055eea0c1eafda891ba08f0c7aab2826ea586c8
MleMoe <i@mle.moe>
2021-10-11 15:47:29 +0800

    :sparkles: add template

---
+++ utils/initApp.ts
@@ -0,0 +1,46 @@
+import chalk from 'chalk';
+import { $, question } from 'zx';
+import * as fs from 'fs';
+
+let appName = await question(chalk.cyanBright('请输入项目名字？'));
+
+// let appId = await question(chalk.blueBright('请输入 app id？'));
+
+// const levelChoices = ['NEWBIE', 'JUNIOR', 'INTERMEDIATE', 'ADVANCED'];
+
+// let level = await question(chalk.cyanBright('请输入难度类别？'), {
+//   choices: [
+//     chalk.cyan('NEWBIE'),
+//     chalk.green('JUNIOR'),
+//     chalk.yellow('INTERMEDIATE'),
+//     chalk.magenta('ADVANCED'),
+//   ].map((e, i) => `${(i + 1).toString()}. ${e}`),
+// });
+
+// console.log(appName);
+// console.log(levelChoices[parseInt(level) - 1]);
+
+const pagesPath = 'pages';
+const pagesFolder = `${pagesPath}/${appName}`;
+
+await $`mkdir ${pagesFolder}`;
+
+const getHtml = (pagaName: string) => `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/src/favicon.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>fe-mentor</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/${pagaName}/main.tsx"></script>
+  </body>
+</html>`;
+
+// await $`echo ${getHtml(appName)} > ${pagesFolder}/index.html`;
+fs.writeFileSync(`${pagesFolder}/index.html`, getHtml(appName));
+
+await $`mkdir src/${appName}`;
+await $`cp -r utils/templates/src/ src/${appName}`;
