df0fb6f06d436d6c5e6f647e7f1370bd29a8b48e
MleMoe <i@mle.moe>
2021-10-12 22:30:18 +0800

    :sparkles: multi-page config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tipCaculator: resolve(__dirname, 'pages/tip-calculator/index.html'),
+      },
+    },
+  },
+});
