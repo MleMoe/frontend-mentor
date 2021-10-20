ce4467e92144a6d395368b70d565a394e94763bd
MleMoe <i@mle.moe>
2021-10-20 16:13:38 +0800

    :sparkles: add author info

--- utils/initApp.mjs
+++ utils/initApp.mjs
@@ -5,7 +5,7 @@ import * as fs from 'fs';
 
 let appName = await question(chalk.cyanBright('请输入项目名字？'));
 
-// let appId = await question(chalk.blueBright('请输入 app id？'));
+let isDark = await question(chalk.blueBright('是暗系视觉风格吗(y/n)？'));
 
 // const levelChoices = ['NEWBIE', 'JUNIOR', 'INTERMEDIATE', 'ADVANCED'];
 
@@ -33,10 +33,35 @@ const getHtml = (pagaName) => `<!DOCTYPE html>
     <link rel="icon" type="image/png" sizes="32x32" href="/src/favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>fe-mentor</title>
+    	<style>
+        .attribution {
+          position: relative;
+          bottom: 10px;
+          font-size: 12px;
+          text-align: center;
+          color: ${isDark === 'y' ? 'hsl(228, 45%, 44%)' : '#fff'};
+        }
+        .attribution a {
+          color: ${isDark === 'y' ? '#fff' : 'hsl(228, 45%, 44%)'};
+          text-decoration: none;
+        }
+      </style>
   </head>
   <body>
     <div id="root"></div>
     <script type="module" src="/src/${pagaName}/index.tsx"></script>
+    <div class="attribution">
+			Challenge by
+			<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+				>Frontend Mentor</a
+			>. Coded by
+			<a href="https://github.com/MleMoe"
+				><img src="/src/icons/github ${
+					isDark === 'y' ? '-light' : ''
+				}-32.png" width="12" height="12" />
+				MleMoe </a
+			>.
+		</div>
   </body>
 </html>`;
 
